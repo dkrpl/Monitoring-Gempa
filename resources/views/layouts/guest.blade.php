db22d40d3879ca60ef1efb52fe11ac0511c25da9
dkrpl <goblind03@gmail.com>
2025-12-06 00:55:52 +0700

    [fixs] favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title', 'EQMonitor - Authentication')</title>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/1995/1995463.png">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('img/logo.svg') }}">
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
